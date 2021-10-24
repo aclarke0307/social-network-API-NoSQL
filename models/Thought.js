873053bd4d977b271adc14f6b196a89ea0c654c8
aclar <a.clarke0307@gmail.com>
2021-10-23 23:50:04 -0400

    reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,35 @@
 const { Schema, model, Types} = require('mongoose');
-const dataFormat = require('../utils/dataFormat');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+
 
 const ThoughtSchema = new Schema(
     {
@@ -28,7 +58,7 @@ const ThoughtSchema = new Schema(
         },
         id: false
     }
-)
+);
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
